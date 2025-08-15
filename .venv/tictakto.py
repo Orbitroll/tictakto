96866085585cfe214a529791e5accb95ef537a5e
Orbitroll <orbitroll@gmail.com>
2025-08-15 20:29:03 +0300

    users

--- .venv/tictakto.py
+++ .venv/tictakto.py
@@ -1,8 +1,14 @@
-board = [['_','_','_'],['_','_','_'],['_','_','_']]
+board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+
+
 def print_boadrd(board):
     for row in board:
         for cell in row:
-            print(f' {cell}' , end= ' ')
+            print(f' {cell}', end=' ')
         print()
 
-print(print_boadrd(board))
+def Users():
+    player_1 =input(f'Please write First user name : ')
+    player_2 =input(f'Please write Secend user name : ')
+
+input(Users())
